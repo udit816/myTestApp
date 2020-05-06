62d47bb4c762c088bce3068f81b4213aefdf9231
Udit Pathak <pathak.udit816@gmail.com>
2020-05-06 17:28:53 +0530

    first commit

---
+++ app/src/main/java/com/uditpathak/mytestapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.uditpathak.mytestapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
